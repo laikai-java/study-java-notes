8240a913f16611bd432e2c35ed884cdb3d2280e2
laikai <787454087@qq.com>
2021-05-13 22:46:49 +0800

    mybatis

---
+++ code/collection/src/main/java/com/lk/collection/collection/list/ListClient.java
@@ -0,0 +1,69 @@
+package com.lk.collection.collection.list;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * list 测试
+ */
+public class ListClient {
+    public static void main(String[] args) {
+        testIterator();
+
+    }
+
+
+    public static ArrayList<String> getArrayList(){
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(""+i);
+        }
+        return list;
+    }
+
+    public static LinkedList<String> getLinkedList(){
+        LinkedList<String> list = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(""+i);
+        }
+        return list;
+    }
+
+    public static void testIterator(){
+
+        ArrayList<String> list = getArrayList();
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        LinkedList<String> linkedList = getLinkedList();
+        Iterator<String> iterator1 = linkedList.iterator();
+        while (iterator1.hasNext()) {
+            System.out.println(iterator1.next());
+        }
+        /**
+         * 内部使用迭代器进行循环
+         * 不能一边循环 一边删除
+         */
+        for(String i : list){
+            list.remove(i);
+        }
+    }
+
+
+    public static void testRemove(){
+        ArrayList<String> list = getArrayList();
+        list.remove(0);
+    }
+
+    public static void testAdd(){
+        ArrayList<String> list = getArrayList();
+        list.add("a");
+        list.add(0,"a");
+
+    }
+}
